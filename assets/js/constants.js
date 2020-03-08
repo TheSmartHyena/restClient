7dc8697a7e50835fd01131eba6bb41858ab25a3f
SkopalPhilippe <skopal.philippe@gmail.com>
2020-03-08 15:57:52 +0100

    1.2
    Tout fonctionne, ma,que le render

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,5 +1,5 @@
 const valid = {
-    contentType: new Set(["application/json", "application/x-www-form-urlencoded", "application/xml"]),
+    contentType: new Set(["application/json", "application/x-www-form-urlencoded"]),
     methods: new Set(["get", "post", "put", "patch", "delete"]),
     language: new Set(["fr", "en", "es", "it", "de"])
 }
